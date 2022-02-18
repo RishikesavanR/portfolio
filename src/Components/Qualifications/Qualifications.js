6c3e2da8c3801c0d321f7de3d384bed219f56cd8
RishikesavanR <rishikesavan2223@gmail.com>
2022-02-18 11:17:43 +0530

    Third commit

--- src/Components/Qualifications/Qualifications.js
+++ src/Components/Qualifications/Qualifications.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Qualifications.css'
 import { Element } from 'react-scroll'
+import {useMediaQuery} from "@chakra-ui/media-query";
 const Qualification = () => {
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)")
   return (
     <Element className='qualifications' name='Qualifications'>
-      <h1 className='qualificationsText' >QUALIFICATIONS</h1>
+      <h1 className='qualificationsText' fontSize={isNotSmallerScreen ? 60:30}>QUALIFICATIONS</h1>
       <h1 className='degree' >BCA</h1>
       <h1 className='expansation'>(Bachelor of Computer Application)</h1>
       <h1 className='year'> Degree completion in 2022</h1>
